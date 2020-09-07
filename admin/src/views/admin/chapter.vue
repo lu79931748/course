11815c417181c20eccccc707ecbd761f80113c85
luwf <luwf@yiche.com>
2020-09-07 16:16:15 +0800

    集成axios完成前后端交互 增加CorsConfig解决跨域问题

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -976,9 +976,17 @@
 export default {
   name: 'chapter',
   mounted: function (){
-    //this.$parent.activeSidebar("business-chapter-sidebar");
+    let _this = this;
+    _this.list();
   },
-  methods: {}
+  methods: {
+    list(){
+      let _this = this;
+      _this.$ajax.get("http://127.0.0.1:9002/business/admin/chapter/list").then((response)=>{
+        console.log("查询大章列表结果：", response);
+      })
+    }
+  }
 }
 </script>
 
