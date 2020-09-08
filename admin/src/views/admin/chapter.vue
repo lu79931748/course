919efe2b1df5d3f21ec5786fe3e40ca529a860f3
luwf <luwf@yiche.com>
2020-09-08 11:32:17 +0800

    添加分页组件pagination.vue

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -1,99 +1,111 @@
 <template>
-  <table id="simple-table" class="table  table-bordered table-hover">
-        <thead>
-        <tr>
+  <div>
+    <p>
+      <button v-on:click="list(1)" class="btn btn-white btn-default btn-round">
+        <i class="ace-icon fa fa-refresh"></i>
+        刷新
+      </button>
+    </p>
+    <pagination ref="pagination" v-bind:list="list" />
+    <table id="simple-table" class="table  table-bordered table-hover">
+      <thead>
+      <tr>
+        <th>ID</th>
+        <th>名称</th>
+        <th>课程ID</th>
+        <th>操作</th>
+      </tr>
+      </thead>
 
-          <th>ID</th>
-          <th>名称</th>
-          <th>课程ID</th>
-          <th>操作</th>
-        </tr>
-        </thead>
+      <tbody>
+      <tr v-for="chapter in chapters">
+        <td>{{ chapter.id }}</td>
+        <td>{{ chapter.name }}</td>
+        <td>{{ chapter.courseId }}</td>
+        <td>
+          <div class="hidden-sm hidden-xs btn-group">
+            <button class="btn btn-xs btn-success">
+              <i class="ace-icon fa fa-check bigger-120"></i>
+            </button>
 
-        <tbody>
-        <tr v-for="chapter in chapters">
-          <td>{{chapter.id}}</td>
-          <td>{{chapter.name}}</td>
-          <td>{{chapter.courseId}}</td>
-          <td>
-            <div class="hidden-sm hidden-xs btn-group">
-              <button class="btn btn-xs btn-success">
-                <i class="ace-icon fa fa-check bigger-120"></i>
+            <button class="btn btn-xs btn-info">
+              <i class="ace-icon fa fa-pencil bigger-120"></i>
+            </button>
+
+            <button class="btn btn-xs btn-danger">
+              <i class="ace-icon fa fa-trash-o bigger-120"></i>
+            </button>
+
+            <button class="btn btn-xs btn-warning">
+              <i class="ace-icon fa fa-flag bigger-120"></i>
+            </button>
+          </div>
+
+          <div class="hidden-md hidden-lg">
+            <div class="inline pos-rel">
+              <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown" data-position="auto">
+                <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
               </button>
 
-              <button class="btn btn-xs btn-info">
-                <i class="ace-icon fa fa-pencil bigger-120"></i>
-              </button>
+              <ul class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
+                <li>
+                  <a href="#" class="tooltip-info" data-rel="tooltip" title="View">
+                                        <span class="blue">
+                                          <i class="ace-icon fa fa-search-plus bigger-120"></i>
+                                        </span>
+                  </a>
+                </li>
 
-              <button class="btn btn-xs btn-danger">
-                <i class="ace-icon fa fa-trash-o bigger-120"></i>
-              </button>
+                <li>
+                  <a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
+                                        <span class="green">
+                                          <i class="ace-icon fa fa-pencil-square-o bigger-120"></i>
+                                        </span>
+                  </a>
+                </li>
 
-              <button class="btn btn-xs btn-warning">
-                <i class="ace-icon fa fa-flag bigger-120"></i>
-              </button>
+                <li>
+                  <a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
+                                        <span class="red">
+                                          <i class="ace-icon fa fa-trash-o bigger-120"></i>
+                                        </span>
+                  </a>
+                </li>
+              </ul>
             </div>
-
-            <div class="hidden-md hidden-lg">
-              <div class="inline pos-rel">
-                <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown" data-position="auto">
-                  <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
-                </button>
-
-                <ul class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
-                  <li>
-                    <a href="#" class="tooltip-info" data-rel="tooltip" title="View">
-																			<span class="blue">
-																				<i class="ace-icon fa fa-search-plus bigger-120"></i>
-																			</span>
-                    </a>
-                  </li>
-
-                  <li>
-                    <a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
-																			<span class="green">
-																				<i class="ace-icon fa fa-pencil-square-o bigger-120"></i>
-																			</span>
-                    </a>
-                  </li>
-
-                  <li>
-                    <a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
-																			<span class="red">
-																				<i class="ace-icon fa fa-trash-o bigger-120"></i>
-																			</span>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </td>
-        </tr>
-        </tbody>
-      </table>
+          </div>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
+import Pagination from "@/components/pagination";
 export default {
   name: 'chapter',
-  data: function (){
-    return{
+  components: {Pagination},
+  data: function () {
+    return {
       chapters: []
     }
   },
-  mounted: function (){
+  mounted: function () {
     let _this = this;
-    _this.list();
+    _this.$refs.pagination.size = 5;
+    _this.list(1);
   },
   methods: {
-    list(){
+    list(page) {
       let _this = this;
       _this.$ajax.post("http://127.0.0.1:9999/business/admin/chapter/list", {
-        page:1,
-        size:1,
-      }).then((response)=>{
+        page: page,
+        size: _this.$refs.pagination.size,
+      }).then((response) => {
         console.log("查询大章列表结果：", response);
         _this.chapters = response.data.list;
+        _this.$refs.pagination.render(page, response.data.total)
       })
     }
   }
