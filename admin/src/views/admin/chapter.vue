094502d9a4b89731e413e239793664edc3995fcf
luwf <luwf@yiche.com>
2020-09-25 11:03:14 +0800

    vue多环境配置

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -117,7 +117,7 @@ export default {
      */
     list(page) {
       let _this = this;
-      _this.$ajax.post("http://127.0.0.1:9999/business/admin/chapter/list", {
+      _this.$ajax.post(process.env.VUE_APP_SERVER + "/business/admin/chapter/list", {
         page: page,
         size: _this.$refs.pagination.size,
       }).then((response) => {
@@ -139,7 +139,7 @@ export default {
         return;
       }
       Loading.show()
-      _this.$ajax.post("http://127.0.0.1:9999/business/admin/chapter/save",
+      _this.$ajax.post(process.env.VUE_APP_SERVER + "/business/admin/chapter/save",
           _this.chapter
       ).then((response) => {
         Loading.hide()
@@ -161,7 +161,7 @@ export default {
       let _this = this;
       Confirm.show("删除后不可恢复，确认删除？", function () {
         Loading.show()
-        _this.$ajax.delete("http://127.0.0.1:9999/business/admin/chapter/delete/" + chapter.id).then((response) => {
+        _this.$ajax.delete(process.env.VUE_APP_SERVER + "/business/admin/chapter/delete/" + chapter.id).then((response) => {
           Loading.hide()
           let resp = response.data;
           if (resp.success) {
