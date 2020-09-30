6668bfca3b9b7ed16d48200fb93588a556a43ee4
luwf <luwf@yiche.com>
2020-09-30 11:37:07 +0800

    生成枚举js

--- admin/src/main.js
+++ admin/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './app.vue'
 import router from './router'
 import axios from 'axios'
-import filter from "@/filter";
+import filter from './filter/filter'
 
 Vue.config.productionTip = false;
 Vue.prototype.$ajax = axios;
