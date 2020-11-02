a2d65968b929dc0a90c52f6bac10d29cd7a04f10
luwf <luwf@yiche.com>
2020-11-02 17:12:52 +0800

    头像优化

--- file/src/main/java/com/course/config/SpringMvcConfig.java
+++ file/src/main/java/com/course/config/SpringMvcConfig.java
@@ -1,5 +1,6 @@
 package com.course.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,9 +11,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
 
+    @Value("${file.path}")
+    private String FILE_PATH;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/f/**").addResourceLocations("file:E:/java/file/course/");
+        registry.addResourceHandler("/f/**").addResourceLocations("file:" + FILE_PATH);
     }
     //http://127.0.0.1:9003/file/f/course/teacher/HNBOaUOW-tupian.jpg
 }
