fb32fb0eddd3039e3ac9fde26703a459420de993
luwf <luwf@yiche.com>
2020-11-16 10:54:39 +0800

    课程管理、小节管理使用上传文件组件

--- admin/src/views/admin/course.vue
+++ admin/src/views/admin/course.vue
@@ -128,7 +128,16 @@
               <div class="form-group">
                 <label class="col-sm-2 control-label">封面</label>
                 <div class="col-sm-10">
-                  <input v-model="course.image" class="form-control">
+                  <file v-bind:id="'image-upload'"
+                        v-bind:text="'上传封面'"
+                        v-bind:suffixs="['jpg', 'jpeg', 'png']"
+                        v-bind:use="FILE_USE.COURSE.key"
+                        v-bind:after-upload="afterUpload"></file>
+                  <div v-show="course.image" class="row">
+                    <div class="col-md-6">
+                      <img v-bind:src="course.image" class="img-responsive">
+                    </div>
+                  </div>
                 </div>
               </div>
               <div class="form-group">
@@ -257,9 +266,11 @@
 </template>
 
 <script>
-  import Pagination from "../../components/pagination";
-  export default {
-    components: {Pagination},
+import Pagination from "../../components/pagination";
+import File from "@/components/file";
+
+export default {
+    components: {Pagination, File},
     name: "business-course",
     data: function() {
       return {
@@ -268,6 +279,7 @@
         COURSE_LEVEL: COURSE_LEVEL,
         COURSE_CHARGE: COURSE_CHARGE,
         COURSE_STATUS: COURSE_STATUS,
+        FILE_USE: FILE_USE,
         categorys: [],
         tree: {},
         saveContentLabel: "",
@@ -545,6 +557,10 @@
           Loading.hide();
           _this.teachers = response.content;
         });
+      },
+      afterUpload(resp){
+        let _this = this;
+        _this.course.image = resp.content.path;
       }
     },
   }
