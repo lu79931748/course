c124584ade8608815c0fceee4983dc7317658900
luwf <luwf@yiche.com>
2020-12-20 20:31:56 +0800

    登录增加验证码校检，chrome请求sessionid不一致后期改用redis缓存

--- admin/src/views/login.vue
+++ admin/src/views/login.vue
@@ -43,7 +43,7 @@
                         <label class="block clearfix">
                           <span class="block input-icon input-icon-right">
                             <div class="input-group">
-                              <input type="text" class="form-control" placeholder="验证码">
+                              <input type="text" v-model="user.imageCode" class="form-control" placeholder="验证码">
                               <span class="input-group-addon" id="basic-addon2">
                                 <img v-on:click="loadImageCode()" id="image-code" alt="验证码"/>
                               </span>
@@ -90,7 +90,8 @@ export default {
   data: function() {
     return {
       user: {},
-      remember: true // 默认勾选记住我
+      remember: true, // 默认勾选记住我
+      imageCodeToken: ""
     }
   },
   mounted: function (){
@@ -113,6 +114,7 @@ export default {
       if(md5 !== rememberUser.md5){
         _this.user.password = hex_md5(_this.user.password + KEY);
       }
+      _this.user.imageCodeToken = _this.imageCodeToken;
       Loading.show();
       _this.$ajax.post(process.env.VUE_APP_SERVER + '/system/admin/user/login', _this.user).then((response)=>{
         Loading.hide();
@@ -137,6 +139,8 @@ export default {
           _this.$router.push("/welcome")
         } else {
           Toast.warning(resp.message)
+          _this.user.password = "";
+          _this.loadImageCode();
         }
       });
     },
