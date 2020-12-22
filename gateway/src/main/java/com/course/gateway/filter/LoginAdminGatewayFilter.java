ec4dfb13641497e47e4b88efaae4b9cde5cf82f8
luwf <luwf@yiche.com>
2020-12-22 14:03:19 +0800

    gateway注解

--- gateway/src/main/java/com/course/gateway/filter/LoginAdminGatewayFilter.java
+++ gateway/src/main/java/com/course/gateway/filter/LoginAdminGatewayFilter.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.core.Ordered;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -15,6 +16,7 @@ import javax.annotation.Resource;
 /**
  * Created by luwf on 2020/12/21
  */
+@Component
 public class LoginAdminGatewayFilter implements GatewayFilter, Ordered{
     private static final Logger LOG = LoggerFactory.getLogger(LoginAdminGatewayFilter.class);
 
--- gateway/src/main/java/com/course/gateway/filter/LoginAdminGatewayFilterFactory.java
+++ gateway/src/main/java/com/course/gateway/filter/LoginAdminGatewayFilterFactory.java
@@ -2,12 +2,14 @@ package com.course.gateway.filter;
 
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 
 /**
  * Created by luwf on 2020/12/21
  */
+@Component
 public class LoginAdminGatewayFilterFactory extends AbstractGatewayFilterFactory<Object>{
 
     @Resource
