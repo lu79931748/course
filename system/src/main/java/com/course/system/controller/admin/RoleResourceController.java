d66c306b3ec468d0f2fc8e4f372f57e7054ebf28
luwf <luwf@yiche.com>
2020-12-30 14:40:20 +0800

    增加校色资源关联表，生成持久层和服务端代码

---
+++ system/src/main/java/com/course/system/controller/admin/RoleResourceController.java
@@ -0,0 +1,65 @@
+package com.course.system.controller.admin;
+
+import com.course.server.domain.RoleResource;
+import com.course.server.dto.RoleResourceDto;
+import com.course.server.dto.PageDto;
+import com.course.server.dto.ResponseDto;
+import com.course.server.exception.ValidatorException;
+import com.course.server.service.RoleResourceService;
+import com.course.server.util.ValidatorUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin/roleResource")
+public class RoleResourceController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RoleResourceController.class);
+    public static final String BUSINESS_NAME = "角色资源关联";
+
+    @Resource
+    private RoleResourceService roleResourceService;
+
+    /**
+     * 列表查询
+     */
+    @RequestMapping("/list")
+    public ResponseDto list(@RequestBody PageDto pageDto) {
+        //前端传入PageDto，后台服务给pageDto设置value，然后返回pageDto
+        ResponseDto responseDto = new ResponseDto();
+        roleResourceService.list(pageDto);
+        responseDto.setContent(pageDto);
+        return responseDto;
+    }
+
+    /**
+     * 保存，id有值时更新，无值时新增
+     */
+    @RequestMapping("/save")
+    public ResponseDto save(@RequestBody RoleResourceDto roleResourceDto) {
+        //保存校检
+        ValidatorUtil.require(roleResourceDto.getRoleId(), "角色");
+        ValidatorUtil.require(roleResourceDto.getResourceId(), "资源");
+
+        ResponseDto responseDto = new ResponseDto();
+        roleResourceService.save(roleResourceDto);
+        responseDto.setContent(roleResourceDto);
+        return responseDto;
+    }
+
+    /**
+     * 删除
+     */
+    @DeleteMapping("/delete/{id}")
+    public ResponseDto delete(@PathVariable String id) {//截取路径对应id
+        LOG.info("id:{}", id);
+        ResponseDto responseDto = new ResponseDto();
+        roleResourceService.delete(id);
+        return responseDto;
+    }
+
+}
