d2e26913adbc616f52fc6585801ad16c809d0576
luwf <luwf@yiche.com>
2021-01-08 16:42:25 +0800

    增加过滤器filter.js、课程等级显示

--- web/src/main.js
+++ web/src/main.js
@@ -2,10 +2,14 @@ import Vue from 'vue'
 import App from './app'
 import router from "./router";
 import axios from 'axios'
+import filter from "./filter/filter";
 
 Vue.config.productionTip = false
 Vue.prototype.$ajax = axios;
 
+Object.keys(filter).forEach(key => {
+  Vue.filter(key, filter[key])
+})
 new Vue({
   router,
   render: h => h(App),
