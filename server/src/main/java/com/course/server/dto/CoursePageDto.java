6b2975697c9cba5079da8e69ed00ec3a42d5c9c0
luwf <luwf@yiche.com>
2021-01-09 23:14:20 +0800

    根据点击的分类进行课程筛选

--- server/src/main/java/com/course/server/dto/CoursePageDto.java
+++ server/src/main/java/com/course/server/dto/CoursePageDto.java
@@ -7,6 +7,8 @@ public class CoursePageDto extends PageDto{
 
     private String status;
 
+    private String categoryId;
+
     public String getStatus() {
         return status;
     }
@@ -15,10 +17,19 @@ public class CoursePageDto extends PageDto{
         this.status = status;
     }
 
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("CoursePageDto{");
         sb.append("status='").append(status).append('\'');
+        sb.append(", categoryId='").append(categoryId).append('\'');
         sb.append(", page=").append(page);
         sb.append(", size=").append(size);
         sb.append(", total=").append(total);
