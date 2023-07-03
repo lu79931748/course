3ce2f56f26a931c35a74331f89ff2337fed01598
luwf <lwf3421@163.com>
2023-07-03 22:46:34 +0800

    更改备案信息

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -5,7 +5,7 @@
           <a href="#">回到顶部</a>
         </p>
         <p>欢迎使用线视频课程系统，程序员的梦工厂，致力于全栈技术推广</p>
-        <a target="_blank" href="https://beian.miit.gov.cn">鲁ICP备19019684号</a>
+        <a target="_blank" href="https://beian.miit.gov.cn">津ICP备2022007587号</a>
       </div>
     </footer>
 </template>
